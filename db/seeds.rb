fad9b8d2f916ef0f56861c8918df76b3fbaa7c76
Lindsay Zhao <lindsayzhao@rndsupoort.net>
2015-12-30 17:43:32 -0500

    add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,38 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Project.destroy_all
 Project.create!([{
-  title: "build a rails project"
+  title: "build a rails project",
+  tasks:[{
+      title: "design the database",
+      content: "make at least 4 tables",
+      status: "start"
+      }]
 },
 {
-  title: "practise devops"
+  title: "practise devops",
+  tasks:[{
+      title: "watch past videos",
+      content: "download first",
+      status: "no"
+      }]
 },
 {
-  title: "make an iphone app"
+  title: "make an iphone app",
+  tasks:[{
+      title: "look at the design",
+      content: "check if it's usable",
+      status: "no"
+      }]
+}
+]);
+Note.destroy_all
+Note.create!([{
+  content: "efficient today"
+},
+{
+  content: "it's snowing"
+},
+{
+  content: "so difficult"
 }
 ]);
