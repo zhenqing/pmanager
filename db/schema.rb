d9430dfb5bb9eb9c37ad3b2321749f3798fb208a
Lindsay Zhao <lindsayzhao@rndsupoort.net>
2015-12-30 15:34:15 -0500

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230202227) do
+ActiveRecord::Schema.define(version: 20151230203234) do
+
+  create_table "notes", force: :cascade do |t|
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
