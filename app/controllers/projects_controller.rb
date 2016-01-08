ff5c30a3b3f32fe50d4c23d96149691cff24b9f5
Lindsay Zhao <lindsayzhao@rndsupoort.net>
2016-01-08 16:43:18 -0500

    add remove task function

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -71,6 +71,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:title, tasks_attributes: [:id, :title, :content, :status])
+      params.require(:project).permit(:title, tasks_attributes: [:id, :title, :content, :status, :_destory])
     end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,8 @@
 class Project < ActiveRecord::Base
     has_many :tasks, dependent: :destroy
     accepts_nested_attributes_for :tasks,
-                                 reject_if: proc{|attributes| attributes['title'].blank?}
+                                 reject_if: proc{|attributes| attributes['title'].blank?},
+                                 allow_destroy: true
     def slug
         title.downcase.gsub(" ","-")
     end
